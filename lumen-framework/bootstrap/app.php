b1299ce1996fe60cb2c7e1cb3032496207ed0a9e
Ian <iancharles901223@gmail.com>
2019-05-10 15:53:03 +0800

    Update app.php

--- lumen-framework/bootstrap/app.php
+++ lumen-framework/bootstrap/app.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
     dirname(__DIR__)
-))->bootstrap();
+))->bootstrap()->init();
 
 /*
 |--------------------------------------------------------------------------
